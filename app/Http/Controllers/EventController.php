213b4e1621abd190dbb639a39a25a2d4e856a444
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-08-23 22:13:47 -0300

    novo

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -55,6 +56,9 @@ class EventController extends Controller
 
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
@@ -63,8 +67,10 @@ class EventController extends Controller
     public function show($id) {
 
         $event = Event::findOrFail($id);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
         
-        return view('events.show', ['event' => $event]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
 
     }
 }
